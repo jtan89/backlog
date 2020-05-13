1dc9afa881cd4006fb74c00b55ebcdd3d9faf81a
John <33640075+jtan89@users.noreply.github.com>
2020-05-13 18:07:59 -0400

    Added Routing and Game Search Edits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BacklogComponent } from './backlog/backlog.component';
 import { GameListComponent } from './backlog/game-list/game-list.component';
 import { GameItemComponent } from './backlog/game-list/game-item/game-item.component';
-import { GameSearchComponent } from './backlog/game-search/game-search.component';
+import { GameSearchComponent } from './game-search/game-search.component';
 import { GameDetailsComponent } from './game-details/game-details.component';
 import { GameSearchService } from './Services/game-search.service';
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'search', component: GameSearchComponent },
+  { path: 'backlog', component: BacklogComponent }
+];
 
 @NgModule({
   declarations: [
@@ -19,12 +29,14 @@ import { GameSearchService } from './Services/game-search.service';
     GameListComponent,
     GameItemComponent,
     GameSearchComponent,
-    GameDetailsComponent
+    GameDetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [GameSearchService],
   bootstrap: [AppComponent]
